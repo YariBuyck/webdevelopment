7527104edd257dd5c270d6290a17778282ca3a42
YariBuyck <yari.buyck@student.vives.be>
2026-03-10 21:56:36 +0100

    commit 16

---
+++ labo16/scripts/spatiesOpConsole.js
@@ -0,0 +1,17 @@
+const setup = () => {
+
+    const maakMetSpaties = (inputText) => {
+        let result = inputText.split("").join(" ");
+        return result;
+    }
+
+    const printWithSpaces = () => {
+        let inputText = document.getElementById("tekst").value;
+        let spacedText = maakMetSpaties(inputText);
+        console.log(spacedText);
+    }
+
+    document.getElementById("printButton").addEventListener("click", printWithSpaces);
+}
+
+window.addEventListener("load", setup);
